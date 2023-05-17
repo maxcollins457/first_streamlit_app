aa37bad131198b9bb669ed89a56112cb7d5fa006
maxcollins457 <72575972+maxcollins457@users.noreply.github.com>
2023-05-17 11:20:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
 
 
 #Stop running whilst troubleshooting
-streamlit.stop()
+# streamlit.stop()
 streamlit.header("The fruit load list contains")
 
 def get_fruit_load_list():
